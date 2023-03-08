504bdb13d4537c2c95f3c27fc3ee2ec1c6e8c618
LeoSallu <L.sallustio99@gmail.com>
2023-03-08 15:39:07 +0100

    Creazione array oggetti

--- Snack-1/js/main.js
+++ Snack-1/js/main.js
@@ -1,2 +1,56 @@
 'use strict';
 
+//Array invitati
+
+const tavoloVip = [
+    {
+        nomeDelTavolo:1,
+        nomeDelOspite:'Brad Pitt',
+        postoOccupato:3
+    },
+    {
+        nomeDelTavolo:1,
+        nomeDelOspite:'Johnny Depp',
+        postoOccupato:1
+    },
+    {
+        nomeDelTavolo:1,
+        nomeDelOspite:'Lady Gaga',
+        postoOccupato:2
+    },
+    {
+        nomeDelTavolo:2,
+        nomeDelOspite:'George Clooney',
+        postoOccupato:3
+    },
+    {
+        nomeDelTavolo:2,
+        nomeDelOspite:'Amal Clooney',
+        postoOccupato:2
+    },
+    {
+        nomeDelTavolo:3,
+        nomeDelOspite:'Cristiano Ronaldo',
+        postoOccupato:1
+    },
+    {
+        nomeDelTavolo:3,
+        nomeDelOspite:'Georgina Rodriguez',
+        postoOccupato:2
+    },
+    {
+        nomeDelTavolo:3,
+        nomeDelOspite:'Chiara Ferragni',
+        postoOccupato:3
+    },
+    {
+        nomeDelTavolo:3,
+        nomeDelOspite:'Fedez',
+        postoOccupato:4
+    },
+    {
+        nomeDelTavolo:1,
+        nomeDelOspite:'Maneskin',
+        postoOccupato:4
+    }
+]
