e872a22499c5a3b31e0e6c3e1aef939b1021b73d
LeoSallu <L.sallustio99@gmail.com>
2023-03-08 16:23:34 +0100

    Creazione Array Studenti

--- Snack-2/js/main.js
+++ Snack-2/js/main.js
@@ -1,1 +1,49 @@
 'use strict';
+
+//Array Studenti
+
+const studenti = [
+    {
+        Id:213,
+        Nome:'MARCO DELLA ROVERE',
+        Voti: 78
+
+    },
+    {
+        Id:110,
+        Nome:'PAOLA CORTELLESSA',
+        Voti: 96
+
+    },
+    {
+        Id:250,
+        Nome:'ANDREA MANTEGNA',
+        Voti: 48
+
+    },
+    {
+        Id:145,
+        Nome:'GAIA BORROMINI',
+        Voti: 74
+
+    },
+    {
+        Id:196,
+        Nome:'LUIGI GRIMALDELLO',
+        Voti: 68
+
+    },
+    {
+        Id:102,
+        Nome:'PIERO DELLA FRANCESCA',
+        Voti: 50
+
+    },
+    {
+        Id:120,
+        Nome:'FRANCESCA DA POLENTA',
+        Voti: 84
+
+    },
+
+];
