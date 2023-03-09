a51925c71e04745e19cbf56767dba7e7be08460d
LeoSallu <L.sallustio99@gmail.com>
2023-03-09 16:24:14 +0100

    array Squadre

--- Snack-4/js/main.js
+++ Snack-4/js/main.js
@@ -1,1 +1,35 @@
 'Use strict';
+
+const serieA =[
+    {
+        Squadra: 'Roma',
+        Punti:0,
+        'Falli Subiti':0
+    },
+    {
+        Squadra: 'Napoli',
+        Punti:0,
+        'Falli Subiti':0
+    },
+    {
+        Squadra: 'Milan',
+        Punti:0,
+        'Falli Subiti':0
+    },
+    {
+        Squadra: 'Inter',
+        Punti:0,
+        'Falli Subiti':0
+    },
+    {
+        Squadra: 'Sassuolo',
+        Punti:0,
+        'Falli Subiti':0
+    },
+    {
+        Squadra: 'Monza',
+        Punti:0,
+        'Falli Subiti':0
+    }
+]
+console.log(serieA);
