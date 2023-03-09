8664d7224e2cc179788c4f0384ad0f58fde2abb6
LeoSallu <L.sallustio99@gmail.com>
2023-03-09 14:19:28 +0100

    Array Bici

--- Snack-3/js/main.js
+++ Snack-3/js/main.js
@@ -1,1 +1,43 @@
 'use strict';
+//Funzioni
+function randomWeight(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+//Array
+const bici = [
+    {
+        nome: 'Franco',
+        colore: 'Rosso',
+        peso: randomWeight(5,30)+' Kg',
+        categoria: 'Da corsa'
+    },
+    {
+        nome: 'Pippo',
+        colore: 'Bianca',
+        peso: randomWeight(5,30)+' Kg',
+        categoria: 'Mountain Bike'
+    },
+    {
+        nome: 'Giuseppe',
+        colore: 'Blu',
+        peso: randomWeight(5,30)+' Kg',
+        categoria: 'City'
+    },
+    {
+        nome: 'Mario',
+        colore: 'Verde',
+        peso: randomWeight(5,30)+' Kg',
+        categoria: 'Da corsa'
+    },
+    {
+        nome: 'Paolo',
+        colore: 'Rosa',
+        peso: randomWeight(5,30)+' Kg',
+        categoria: 'City'
+    },
+]
+bici.forEach(element => {
+    console.log(element);
+});
